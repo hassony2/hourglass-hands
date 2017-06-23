ac73c6bbac60748455e95555aa3af6c6507a79a4
Yana Hasson  "WILLOW <yhasson@sequoia.cm.cluster>
2017-06-23 17:04:53 +0200

    Add qsub scripts

---
+++ src/launch_training.sh
@@ -0,0 +1,6 @@
+echo script
+echo $HOSTNAME
+echo $PATH
+th main.lua -expID test-run -trainBatch 2 -trainIters 4000 -validBatch 2 -validIters 1000 -nEpochs 10 -saveHeatmaps -saveInput -finalPredictions -dataDir /sequoia/data1/yhasson/hourglass-hands/data/annots
+
+
